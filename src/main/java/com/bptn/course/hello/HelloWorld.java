eca5f22846d1ef312449f049b2dda137db411f27
Callistus <c.o.enekhaze@gmail.com>
2024-09-25 17:53:27 -0400

    added HelloWorld.java

---
+++ src/main/java/com/bptn/course/hello/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.bptn.course.hello;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// printing out a simple code that says Hello World
+		System.out.println("Hello World");
+	}
+
+}
